74db40dc9a1c3bea105a2e02130362b71b7e8cd0
Naru Designs <ibeabuchipaul@gmail.com>
2020-07-13 03:32:40 +0100

    complete with no issues

---
+++ app.js
@@ -0,0 +1,28 @@
+let uploadBtn = document.getElementById('upload-btn');
+let message = document.getElementById('message');
+let cloudsContainer = document.getElementById('clouds-container');
+let clouds = document.getElementsByClassName('cloud');
+let cloudComponents = document.getElementsByClassName('cloud-components');
+let particles = document.getElementsByClassName('particles');
+
+function animate() {
+    message.classList.add("fade-out");
+    setTimeout(() => {
+        clouds[0].classList.add("slide-LR");
+        clouds[1].classList.add("slide-up-down");
+        clouds[2].classList.add("slide-RL");
+        for (let i=0; i<particles.length; i++){
+            particles[i].classList.add("slide-up");
+        }
+        for (let i=0; i<cloudComponents.length; i++){
+            cloudComponents[i].classList.add("to-blue-color");
+        }
+    }, 600);
+    setTimeout(() => {
+        message.innerText = "Done";
+        message.classList.add("fade-in");
+        cloudsContainer.classList.add("fade-out");
+    }, 5000);
+}
+
+uploadBtn.addEventListener("click", animate);
